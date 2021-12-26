da8f17f3ebc79e7d0d09c24677ea07a950f0339f
Zahra <abbaszadeh.zhr@gmail.com>
2021-12-26 23:37:12 +0330

    First Question Solved

---
+++ 2/2.js
@@ -0,0 +1,8 @@
+// Let circy=new Circle(11)
+// Circy.getArea()
+//should return 380.132711084365
+/////////////////////////////////////////////
+// Let circy =new Circle(4.44)
+// Circy.getPerimeter()
+//should return 27.897342763877365
+
